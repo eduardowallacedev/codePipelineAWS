7445b1923409a532296819878a54315755a559ff
eduardowallacedev <eduardo.wallace@outlook.com.br>
2020-09-01 22:17:38 -0300

    Versão 2

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA! o/"
+    return "<h1>Hola FIAP!</h1>\nMBA! o/ Versão 2 com deploy pela AWS, usando codepipeline e beanstalk"
 
 if __name__ == '__main__':
     application.run()
